9d822f8a97a7569c9d91640915a71d0ad42e6321
John Hawkins <hawkjo@gmail.com>
2014-11-03 18:27:21 -0600

    test_knownToRefSeq.py -> pep8 compliant

--- test_knownToRefSeq.py
+++ test_knownToRefSeq.py
@@ -1,9 +1,9 @@
-import sys, os
+import os
 from itertools import izip
 from collections import defaultdict
 
-NCBI_DATA_dir  = '/home/hawkjo/scratch/dbs/NCBI/DATA/'
-gene2refseq_file = os.path.join(NCBI_DATA_dir, 'gene2refseq') 
+NCBI_DATA_dir = '/home/hawkjo/scratch/dbs/NCBI/DATA/'
+gene2refseq_file = os.path.join(NCBI_DATA_dir, 'gene2refseq')
 
 print 'Reading field names...'
 with open(gene2refseq_file) as f:
@@ -14,7 +14,7 @@ print 'Reading gene2refseq...'
 field_given_value = {}
 for line in open(gene2refseq_file):
     values = line.strip().split('\t')
-    for field_name, value in izip( gene2refseq_fields, values ):
+    for field_name, value in izip(gene2refseq_fields, values):
         field_given_value[value.split('.')[0]] = field_name
 
 print 'Reading kg2refseq...'
@@ -27,12 +27,12 @@ failures = 0
 print 'Failures:'
 for rs in refseqs_in_kg:
     try:
-        counts[ field_given_value[rs] ] += 1
+        counts[field_given_value[rs]] += 1
     except KeyError:
         print rs
         failures += 1
 
 print
 print '%7d  KeyErrors' % failures
-for k,v in sorted(counts.items(), key=lambda tup:-tup[1]):
+for k, v in sorted(counts.items(), key=lambda tup: -tup[1]):
     print '%7d  %s' % (v, k)
